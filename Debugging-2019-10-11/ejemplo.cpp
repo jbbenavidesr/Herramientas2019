04268b3dcf02647a72a346e93496f1611976a477
Juan Bernardo Benavides <jbbenavidesr@unal.edu.co>
2019-10-11 21:03:17 -0500

    Debugging mac fail...

---
+++ Debugging-2019-10-11/ejemplo.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char **argv)
+{
+    // declare variables
+    const int N = 10;
+    double * array;
+    //reserve memory
+    array = new double [N];
+    // initialise array
+    for (int ii = 0; ii < N; ++ii) {
+        array[ii] = 2*(++ii); // != 2*(ii++) ? 
+     }
+    // print some values 
+    int idx = 2;
+    std::cout << array[idx] << std::endl;
+    idx = 10;
+    //std::cout << array[idx] << std::endl; // compiles, but ... 
+
+    std::cout << 1/0 << std::endl; 
+
+    // free memory
+    delete [] array;
+
+    return EXIT_SUCCESS;
+}
